13c6f71a50fe0015b0c9e4bbbf9e712aed46440b
Ali Ahmed <ali.ahmed@berkeley.edu>
2017-05-14 02:48:32 -0400

    changed filed structure again

---
+++ other_files/APIs_with_Python-master/README.md
@@ -0,0 +1,7 @@
+# APIs_with_Python
+
+Notebooks giving examples for working with the following APIS
+
+-  Open-Notify [website](http://open-notify.org/)
+- Github [website](https://github.com/)
+- Reddit [website](https://reddit.com)
---
+++ other_files/APIs_with_Python-master/tweet_stuff/settings.py
@@ -0,0 +1,10 @@
+TWITTER_APP_KEY, TWITTER_APP_SECRET = "zS2Ex9moP635A6Nu5G6uJfaPu", "towF0X2m59jtFRwudf2X1edKbpTEXXneZD6tDLo0FDiGJCxXFH"
+TWITTER_KEY, TWITTER_SECRET = "796875093456494592-XTA7Khp5LZVErrvUE30fkhnGAPhuQk3", "kArRkH2zld4b68qtMyr9KzRb5alyRCyLPsUpAGwYDZX4w"
+
+
+
+
+TRACK_TERMS = ["trump", "clinton", "hillary clinton", "donald trump"]
+CONNECTION_STRING = "sqlite:///tweets.db"
+CSV_NAME = "tweets.csv"
+TABLE_NAME = "tweets"
