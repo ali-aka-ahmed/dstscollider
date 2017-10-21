c7df15f990599951b92210826d1bb5dbb99a0e49
Ali Ahmed <ali.ahmed@berkeley.edu>
2017-10-20 21:30:21 -0700

    yolo

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ import os
 import sys
 from py2neo import Graph
 
+import os
+import sys
+import time
+import requests
+import keys
+from py2neo import Graph, Relationship, Node
+
 ################# NOTES ##################
 # add alerts for exam schedule conflicts #
 ##########################################
@@ -21,10 +28,18 @@ def graph():
 	search = request.form["twitter_search_term"]
 	number_objects = request.form["number_objects"]
 
-	graphenedb_url = os.environ.get("GRAPHENEDB_BOLT_URL")
-	graphenedb_user = os.environ.get("GRAPHENEDB_BOLT_USER")
-	graphenedb_pass = os.environ.get("GRAPHENEDB_BOLT_PASSWORD")
-	graph = Graph(graphenedb_url, user=graphenedb_user, password=graphenedb_pass, bolt = True, secure = True, http_port = 24789, https_port = 24780)
+	# graphenedb_url = os.environ.get("GRAPHENEDB_BOLT_URL")
+	# graphenedb_user = os.environ.get("GRAPHENEDB_BOLT_USER")
+	# graphenedb_pass = os.environ.get("GRAPHENEDB_BOLT_PASSWORD")
+	# graph = Graph(graphenedb_url, user=graphenedb_user, password=graphenedb_pass, bolt = True, secure = True, http_port = 24789, https_port = 24780)
+	graph = Graph("http://" + 'neo4j' + ":" + 'Berkatores19!' + "@localhost:7474/browser/")
+	#possibly run detach to delete all nodes
+
+	def constraint(label, property):
+		schema = graph.schema
+		if property not in schema.get_uniqueness_constraints(label):
+			schema.create_uniqueness_constraint(label, property)
+	
 
 	constraint("Tweet", "id")
 	constraint("User", "username")
@@ -32,14 +47,96 @@ def graph():
 
 	since_id = -1
 	
+
+	def find_tweets(keyword, cnt, since_id=-1):
+		TWITTER_BEARER = keys.twitter_bearer
+		
+		headers = dict(
+		  accept = 'application/json',
+		  Authorization='Bearer ' + TWITTER_BEARER
+		)
+		
+		payload = dict(
+		  q= keyword,
+		  count = cnt,
+		  result_type = "recent",
+		  lang = "en",
+		  since_id = since_id
+		)
+		
+		base_url = "https://api.twitter.com/1.1/search/tweets.json?"
+		url = base_url + "q={q}&count={count}&result_type={result_type}&lang={lang}&since_id={since_id}".format(**payload)
+		
+		r = requests.get(url, headers=headers)
+		tweets = r.json()["statuses"]
+
+		return tweets
+
+
+	def upload_tweets(tweets):
+		for t in tweets:
+			u = t['user']
+			e = t['entities']
+
+			tweet = Node("Tweet", id=t['id'])
+			graph.merge(tweet)
+			tweet['text'] = t['text']
+			tweet['id'] = t['id']
+			tweet.push()
+
+			user = Node("User", id=u['screen_name'])
+			graph.merge(user)
+			user['username'] = u['screen_name']
+			user.push()
+
+			graph.create(Relationship(user, "POSTS", tweet))
+
+			for h in e.get('hashtags', []):
+				hashtag = Node("Hashtag", id=h['text'].lower())
+				graph.merge(hashtag)
+				hashtag['name'] = h['text'].lower() #Hashtag searches are not case-sensitive
+				hashtag.push()
+
+				graph.create(Relationship(hashtag, "TAGS", tweet))
+
+
+			for m in e.get('user_mentions', []):
+				mention = Node("User", id=m['screen_name'])
+				graph.merge(mention)
+				mention['username'] = m['screen_name']
+				mention.push()
+
+				graph.create(Relationship(tweet, "MENTIONS", mention))
+
+			reply = t.get('in_reply_to_status_id')
+			if reply:
+				reply_tweet = Node("Tweet", id=reply)
+				graph.merge(reply_tweet)
+				reply_tweet["id"] = reply
+				reply_tweet.push()
+		
+				graph.create(Relationship(tweet, "REPLY_TO", reply_tweet))
+
+			r = t.get('retweeted_status', {})
+			r = r.get('id')
+
+			if r:
+				retweet = Node("Tweet", id=r)
+				graph.merge(retweet)
+				retweet["id"] = r
+				retweet.push()
+			
+				graph.create(Relationship(tweet, "RETWEETS", retweet))
+
+
 	try:
 		tweets = find_tweets(search, number_objects, since_id=since_id)
 		since_id = tweets[0].get('id')
 		upload_tweets(tweets)
 	except Exception as e:
-		console.log(e)
+		return render_template('error.html', data=e)
 
-	result = graph.run("MATCH (n) RETURN (n)")
+	#it finished loading things into the database. now we need to pull it out and render using d3
 
 	return render_template('index.html', data=result)
 
